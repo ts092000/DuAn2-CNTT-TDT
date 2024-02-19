96a1843bcbf6c91c20538e26a8fd6e116a9ca321
ts092000 <ts092000@gmail.com>
2024-02-19 20:22:06 +0700

    Update login and sign up UI

---
+++ src/Components/LoginSignUp/LoginSignUp.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import './LoginSignUp.css';
+
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+import tdt_logo from '../Assets/logoTDT.png'
+
+const LoginSignUp = () => {
+    const [action, setAction] = useState("Login");
+
+    return (
+        <div className='container'>
+            <div className="logo">
+                <img src={tdt_logo} alt="" />
+            </div>
+            <div className="header">
+                <div className="text">{action}</div>
+                <div className="underline"></div>
+            </div>
+
+            <div className="inputs">
+                {action==="Login"?<div></div>:<div className="input">
+                    <img src={user_icon} alt="" />
+                    <input type="text" placeholder="Name"/>
+                </div>}
+                
+                <div className="input">
+                    <img src={email_icon} alt="" />
+                    <input type="email" placeholder="Email"/>
+                </div>
+                <div className="input">
+                    <img src={password_icon} alt="" />
+                    <input type="password" placeholder="Password"/>
+                </div>
+            </div>
+
+            {action==="Sign Up"?<div></div>:<div className="forgot-password">
+                Forgot your password?
+            </div>}
+
+            <div className="submit-container">
+                <div className={action==="Login"?"submit gray":"submit"} 
+                    onClick={() => {
+                        setAction("Sign Up")
+                    }}>
+                    Sign Up
+                </div>
+                <div className={action==="Sign Up"?"submit gray":"submit"}
+                    onClick={() => {
+                        setAction("Login")
+                    }}>
+                    Login
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginSignUp
